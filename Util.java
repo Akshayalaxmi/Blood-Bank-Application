eabfb919c159577d9f7515725a3ef429d62923a4
Akshayalaxmi <94473078+Akshayalaxmi@users.noreply.github.com>
2022-09-04 19:52:54 +0530

    Add files via upload

---
+++ Util.java
@@ -0,0 +1,7 @@
+package com.example.ex1.Email;
+
+public class Util {
+
+    public static final String EMAIL = "meghanashetty2804@gmail.com";
+    public static final String PASSWORD = "qbcygishstakormp";
+}
